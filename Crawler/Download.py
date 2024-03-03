a523072402ec7427f84e8b70ea5f51e4dec1eff2
shangyu <oraoraora947@gmail.com>
2024-03-04 02:53:42 +0800

    fix crawl imgur no image

--- Crawler/Download.py
+++ Crawler/Download.py
@@ -1,22 +1,29 @@
 import os
 import re
 import requests
-import time
 from tqdm import tqdm
 from .PttArticle import ArticleInfo
 
 
 def Download(Article: ArticleInfo) -> None:
-    if not os.path.exists(Article.title):
-        os.mkdir(Article.title)
+    if not Article.title:
+        title = '沒有標題'
+    else:
+        title = Article.title.replace('/', '_')
+    
+    os.makedirs(title, exist_ok=True)
 
-    urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', Article.content)
-    jpg_urls = [url for url in urls if url.endswith('.jpg') or url.endswith('.jpeg')]
+    urls = re.findall(r"https?://[^'\"\s]+" , Article.content)
+    jpg_urls = [url.strip() for url in urls if url.endswith('.jpg') or url.endswith('.jpeg')]
         
     index = 0
     for url in tqdm(jpg_urls, desc = '下載圖片'):
-        img = requests.get(url)
-        with open(os.path.join(Article.title, f"{index+1}.jpg"), "wb") as file: 
+        headers = headers = {'content-type': 'text/html; charset=UTF-8', 
+                             'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+        }
+        img = requests.get(url, headers = headers)
+        with open(os.path.join(title, f"圖{index+1}.jpg"), "wb") as file: 
+            print(img.content)
             file.write(img.content)
         index += 1
 
